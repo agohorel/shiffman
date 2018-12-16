cf80fe2a9e3d9d59e199260e56170b03156dd8f7
agohorel <alex.oddlogic@gmail.com>
2018-12-16 14:16:42 -0500

    sort by H/S/B separately, at once

--- colorSort.js
+++ colorSort.js
@@ -1,8 +1,7 @@
-let values = [];
-let hIterator = 0,
-	sIterator = 0,
-	bIterator = 0,
-	brightnessIterator = 0;
+let colors = [], colors2, colors3;
+let iterator = {val: 0};
+let iterator2 = {val: 0};
+let iterator3 = {val: 0};
 
 function setup() {
 	createCanvas(800, 500);
@@ -12,69 +11,23 @@ function setup() {
 		let h = int(random(255));
 		let s = int(random(255));
 		let b = int(random(255));
-		values.push(color(h, s, b));
+		colors.push(color(h, s, b));
 	}
+
+	colors2 = colors.slice();
+	colors3 = colors.slice();
 }
 
 function draw() {
-
-	if (hIterator < values.length) {
-		for (let j = 0; j < values.length - hIterator - 1; j++) {
-			let a = values[j];
-			let b = values[j + 1];
-
-			if (getHue(a) > getHue(b)){
-				swap(values, j, j + 1);
-			}
-		}
-		hIterator++;
-		if (hIterator === values.length){
-			print("finished sorting by hue");
-		} 
-	} 
-
-	else if (sIterator < values.length) {
-		for (let j = 0; j < values.length - sIterator - 1; j++) {
-			let a = values[j];
-			let b = values[j + 1];
-
-			if (getSaturation(a) > getSaturation(b)){
-				swap(values, j, j + 1);
-			}
-		}
-		sIterator++;
-		if (sIterator === values.length){
-			print("finished sorting by saturation");
-		} 
-	}
-
-	else if (bIterator < values.length) {
-		for (let j = 0; j < values.length - bIterator - 1; j++) {
-			let a = values[j];
-			let b = values[j + 1];
-
-			if (getBrightness(a) > getBrightness(b)){
-				swap(values, j, j + 1);
-			}
-		}
-		bIterator++;
- 		if (bIterator === values.length){
-			print("finished sorting by brightness");
-		} 
+	if (iterator.val < colors.length) {
+		runSort(colors, iterator, "hue", getHue, 0, height/3);
+		runSort(colors2, iterator2, "saturation", getSaturation, height/3, height - height/3);
+		runSort(colors3, iterator3, "brightness", getBrightness, height - height/3, height);
 	}
 
 	else {
 		print("finished");
-		// reset to zero (and comment out noLoop()) if you want to loop infinitely
-		// hIterator = 0;
-		// sIterator = 0;
-		// bIterator = 0;
 		noLoop();
-	}
-
-	for (let i = 0; i < values.length; i++) {
-		stroke(values[i]); // private i
-		line(i, 0, i, height);
 	}
 }
 
@@ -96,9 +49,22 @@ function getBrightness(color){
 	return color._getBrightness();
 }
 
-function getAvgDiff(a, b){
-	let diffR = a[0] - b[0];
-	let diffG = a[1] - b[1];
-	let diffB = a[2] - b[2];
-	return diffR + diffG + diffG / 3;
+function runSort(array, iterator, sortingMethod, fn, yStart, yEnd){
+	for (let j = 0; j < array.length - iterator.val - 1; j++) {
+			let a = array[j];
+			let b = array[j + 1];
+
+			if (fn(a) > fn(b)){
+				swap(array, j, j + 1);
+			}
+		}
+	iterator.val++;
+	if (iterator.val === array.length){
+		print(`finished sorting by ${sortingMethod}`);
+	}
+
+	for (let i = 0; i < array.length; i++) {
+		stroke(array[i]);
+		line(i, yStart, i, yEnd);
+	}
 }
