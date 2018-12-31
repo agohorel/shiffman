e5a381ffb1faabee0a5ada00d55770a9ab3d73de
agohorel <alex.oddlogic@gmail.com>
2018-12-31 15:46:17 -0500

    attempt to sort by distance
    in progress/kinda borked

--- color sort/colorSort.js
+++ color sort/colorSort.js
@@ -1,32 +1,36 @@
-p5.disableFriendlyErrors = true;
+// p5.disableFriendlyErrors = true;
 
-let cnv;
 let colors = [], colors2, colors3;
+let colorVectors = [];
+let distances = [];
 let iterator = {val: 0};
 let iterator2 = {val: 0};
 let iterator3 = {val: 0};
-let scale = 3;
+let scale = 2;
 
 function setup() {
-	cnv = createCanvas(windowWidth, windowHeight);
+	createCanvas(windowWidth, windowHeight);
 	colorMode(HSB);
 
 	for (let i = 0; i < width/scale; i++) {
-		let h = int(random(180, 360));
-		let s = int(random(100));
-		let b = int(random(100));
+		let h = int(random(360));
+		let s = int(100);
+		let b = int(100);
 		colors.push(color(h, s, b));
+		colorVectors.push(createVector(h, s, b));
 	}
-
-	colors2 = colors.slice();
-	colors3 = colors.slice();
+	// colors2 = colors.slice();
+	// colors3 = colors.slice();
+	// getDistances(colorVectors);
 }
 
 function draw() {
 	if (iterator.val < colors.length) {
-		bubbleSort(colors2, iterator2, "saturation", getSaturation, 0, height/3);
-		bubbleSort(colors, iterator, "hue", getHue,height/3, height - height/3);
-		bubbleSort(colors3, iterator3, "brightness", getBrightness, height - height/3, height);
+		// bubbleSort(colors2, iterator2, "saturation", getSaturation, 0, height/3);
+		// bubbleSort(colors, iterator, "hue", getHue,height/3, height - height/3);
+		// bubbleSort(colors3, iterator3, "brightness", getBrightness, height - height/3, height);
+		getDistances(colorVectors);
+		bubbleSort(colors, colorVectors, distances, iterator, "vector", 0, height);
 	}
 
 	else {
@@ -63,18 +67,48 @@ function drawLines(array, yStart, yEnd){
 	}
 }
 
-function bubbleSort(array, iterator, sortingMethod, fn, yStart, yEnd){
-	for (let j = 0; j < array.length - iterator.val - 1; j++) {
-			let a = array[j];
-			let b = array[j + 1];
+// function bubbleSort(array, iterator, sortingMethod, fn, yStart, yEnd){
+// 	for (let j = 0; j < array.length - iterator.val - 1; j++) {
+// 			let a = array[j];
+// 			let b = array[j + 1];
 
-			if (fn(a) > fn(b)){
-				swap(array, j, j + 1);
+// 			if (fn(a) > fn(b)){
+// 				swap(array, j, j + 1);
+// 			}
+// 		}
+// 	iterator.val++;
+// 	if (iterator.val === array.length){
+// 		print(`finished sorting by ${sortingMethod}`);
+// 	}
+// 	drawLines(array, yStart, yEnd);
+// }
+
+function bubbleSort(colors, colorVectors, distances, iterator, sortingMethod, yStart, yEnd){
+	
+	for (let j = 0; j < colors.length - iterator.val - 1; j++) {
+			let a = distances[j];
+			let b = distances[j+1];
+
+			if (a > b){
+				swap(colors, j, j+1);
+				swap(colorVectors, j, j+1);
+				// swap(distances, j, j+1);
 			}
 		}
 	iterator.val++;
-	if (iterator.val === array.length){
+	if (iterator.val === colors.length){
 		print(`finished sorting by ${sortingMethod}`);
 	}
-	drawLines(array, yStart, yEnd);
+	drawLines(colors, yStart, yEnd);
+}
+
+function getDistances(colorVectors){
+	distances = [];
+	for (let i = 0; i < colorVectors.length-1; i++) {
+		let a = colorVectors[i];
+		let b = colorVectors[i+1];
+		let dist = a.dist(b);
+		distances.push(dist);
+	}
+	print(distances);
 }
