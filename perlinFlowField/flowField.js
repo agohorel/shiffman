848e7d45bcf49cfaedba7f6caca6e69afb099d76
agohorel <alex.oddlogic@gmail.com>
2019-03-07 14:04:37 -0500

    tweak behavior, cleanup & comment code

--- perlinFlowField/flowField.js
+++ perlinFlowField/flowField.js
@@ -15,8 +15,10 @@ function setup() {
 	cols = floor(width / scale);
 	rows = floor(height / scale);
 
+	// create empty array to store vectors in
 	flowField = new Array(cols * rows);
 
+	// instantiate particle class
 	for (let i = 0; i < particles.length; i++) {
 		particles[i] = new Particle();
 	}
@@ -25,19 +27,25 @@ function setup() {
 function draw() {
 	let xOffset = 0;
 	for (let x = 0; x < cols; x++) {
+		// reset yOffset each loop thru column
 		let yOffset = 0;
 		for (let y = 0; y < rows; y++) {
-			let angle = noise(xOffset, yOffset, zOffset) * TWO_PI * 4;
+			// the " * 4" is to add more chaos to the angles
+			let angle = noise(xOffset, yOffset, zOffset) * TWO_PI * 2;
+			// get the index of current cell
 			let index = x + y * cols;
+			// create vector using angle var above
 			let v = p5.Vector.fromAngle(angle);
 
+			// set random magnitudes to add variation
 			v.setMag(random(10));
+			// set vectors in our stored array 
 			flowField[index] = v;
 			yOffset += increment;
-			// showNoiseField(x, y, v);
 		}
 		xOffset += increment;
-		zOffset += increment * 0.0001;
+		// increment "time" more slowly
+		zOffset += increment * 0.001;
 	}
 	
 
@@ -45,7 +53,6 @@ function draw() {
 		particles[i].follow(flowField);
 		particles[i].update();
 		particles[i].edges();
-		// particles[i].updatePrevPos();
 		particles[i].display();
 	}
 
@@ -56,16 +63,17 @@ function Particle() {
 	this.vel = createVector(0, 0);
 	this.acc = createVector(0, 0);
 	this.maxSpeed = random(1, 6);
-	// this.prevPos = this.pos.copy();
 	this.thickness = 1;
 
 	this.follow = function(vectors) {
-		// get the grid section for each vector
+		// get the grid coords of nearest vector
 		let x = floor(this.pos.x / scale);
 		let y = floor(this.pos.y / scale);
-		// get the index of a given grid/cell
+		// get the index of nearest vector
 		let index = x + y * cols;
+		// get nearest flowField vector
 		let force = vectors[index];
+		// apply flowField vector as force on particle
 		this.applyForce(force);
 	}
 
@@ -81,32 +89,11 @@ function Particle() {
 		this.thickness = this.vel.x + this.vel.y * .0001;
 	}
 
-	// all this prevPos stuff seems to be no different than without. cut it? 
-	// this.updatePrevPos = function() {
-	// 	this.prevPos.x = this.pos.x;
-	// 	this.prevPos.y = this.pos.y;
-	// }
-
 	this.edges = function() {
-		if (this.pos.x > width) {
-			this.pos.x = 0;
-			// this.updatePrevPos();
-		}
-
-		if (this.pos.y > height) {
-			this.pos.y = 0;
-			// this.updatePrevPos();
-		}
-
-		if (this.pos.x < 0) {
-			this.pos.x = width;
-			// this.updatePrevPos();
-		}
-
-		if (this.pos.y < 0) {
-			this.pos.y = height;
-			// this.updatePrevPos();
-		}
+		if (this.pos.x > width) this.pos.x = 0;
+		if (this.pos.y > height) this.pos.y = 0;
+		if (this.pos.x < 0) this.pos.x = width;
+		if (this.pos.y < 0) this.pos.y = height;
 	}
 
 	this.display = function() {
@@ -114,19 +101,12 @@ function Particle() {
 		let yScaled = map(this.pos.y, 0, height, 0, 180);
 		let variation = random(-20, 20);
 
-		// stroke(xScaled + variation, 0 + variation, yScaled + variation, xScaled + yScaled * .001);
-		stroke((xScaled + yScaled) + variation, 
+		strokeWeight(this.thickness);
+		stroke((xScaled + yScaled * .2) + variation, 
 			    xScaled + variation * .25, 
 			    yScaled + variation * .25, 
 			    xScaled + yScaled * .5);
 		
-		strokeWeight(this.thickness);
-		// line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
-		// line(width - this.prevPos.x, height - this.prevPos.y, width - this.pos.x, height - this.pos.y);
-		// this.updatePrevPos();
-
-		// point(this.pos.x, this.pos.y);
-		// point(width - this.pos.x, height - this.pos.y);
 
 		line(this.pos.x, this.pos.y, this.pos.x, this.pos.y);
 		line(width - this.pos.x, height - this.pos.y, width - this.pos.x, height - this.pos.y);
